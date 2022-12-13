990c2e7240abfafa38d2eea4d5e1ec4145e56536
Bagasaditama123 <Bagas_aditama@teknokrat.ac.id>
2022-12-13 20:38:07 +0700

    bagasadi: upload controller obat

---
+++ Server/application/controllers/obat.php
@@ -0,0 +1,96 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH."libraries/Server.php";
+class obat extends Server {
+	
+	public function __construct()
+	{
+			parent::__construct();
+			
+			$this->load->model("Models_obat","model",TRUE);
+	}
+
+	
+	function service_get(){
+		
+		$this->load->model("Models_obat","model",TRUE);
+		
+		$hasil = $this->model->get_data();
+		
+		$this->response(array("obat" => $hasil),200);
+	}
+
+	
+	function service_put(){
+		
+		$this->load->model("Models_obat", "model", TRUE);
+		
+		$data = array(
+			"kode" => $this->put("kode"),
+			"nama" => $this->put("nama"),
+			"jenis" => $this->put("jenis"),
+			"harga" => $this->put("harga"),
+            "stok" => $this->put("stok"),
+			"token" => base64_encode($this->put("token")),
+		);
+
+		
+		$hasil = $this->model->update_data($data["kode"],$data["nama"],$data["jenis"],$data["harga"],$data["stok"]);
+		
+		
+		if($hasil == 0){
+			$this->response(array("status"=>"Data obat Berhasil Diubah"),200);
+		}
+		
+		else{
+			$this->response(array("status"=>"Data obat Gagal Diubah!"),200);
+		}
+	}
+
+	
+	function service_post(){
+		
+		$this->load->model("Models.php", "model", TRUE);
+		
+		$data = array(
+			"kode" => $this->post("kode"),
+			"nama" => $this->post("nama"),
+			"jenis" => $this->post("jenis"),
+			"harga" => $this->post("harga"),
+            "stok" => $this->put("stok"),
+			"token" => base64_encode($this->post("token")),
+		);
+		
+		$hasil = $this->model->save_data($data["kode"],$data["nama"],$data["jenis"],$data["harga"],$data["stok"]);
+	
+		if($hasil == 0){
+			$this->response(array("status"=>"Data obat Berhasil Disimpan"),200);
+		}
+		
+		else{
+			$this->response(array("status"=>"Data obat Gagal Disimpan!"),200);
+		}
+	}
+	
+	
+	function service_delete(){
+
+		$this->load->model("Models_obat", "model", TRUE);
+
+		$token = $this->delete("kode");
+
+		$hasil = $this->model->delete_data($token);
+
+		if($hasil == 1)
+		{
+
+			$this->response(array("status"=>"Data obat berhasil dihapus"),200);
+		}
+		else
+		{
+			$this->response(array("status" => "Data obat gagal dihapus !"), 200);
+		}
+	}
+    
+}
